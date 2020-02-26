e1a2fd63e1e7759611abda71bc55d065ac407308
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-26 14:30:09 -0600

    Update problem 6 - factorial.py

--- problem 6 - factorial.py
+++ problem 6 - factorial.py
@@ -5,6 +5,8 @@
 import math
 number = int(input("Enter a number: "))
 factorial = 1
+#no need for int here. It is already declared above.
+#use (1, number +1)
 for i in range (1,int(number)+1):
     factorial = factorial * i
 print("Factorail is",factorial)
