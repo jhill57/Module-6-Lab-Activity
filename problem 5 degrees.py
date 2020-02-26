0b28064a930b90afa63c972a0f7dd3b13d2b55ba
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-26 14:24:47 -0600

    Update problem 5 degrees.py

--- problem 5 degrees.py
+++ problem 5 degrees.py
@@ -4,8 +4,11 @@
 import math
 radians = int(input("How many radians?"))
 degrees = radians*(180/3.14)
-print("non-calculated = ",degrees)
-
+print("non-calculated = ",degrees) # this returns a calculated value.
+#The calculations below didn't come out correctly.
+#Use:  
+#print("Python math.degrees calculated degrees = ", math.degrees(radians))
+#The two calculations should return same results.
 degrees_python = math.degrees(1)
 print("degrees function = ",degrees_python)
 
