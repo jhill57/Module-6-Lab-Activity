4ee2633165df151d5975505c5f0d2a97dca87258
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-26 14:13:13 -0600

    Update problem 1 - randomrange.py

--- problem 1 - randomrange.py
+++ problem 1 - randomrange.py
@@ -3,6 +3,10 @@
 # this code prints a random range
 
 import random
+#Use the code below to get 10 random numbers within the range.
+#for i in range(1, 11):
+#    print(random.randrange(25, 36)) NOTE:  to include 35, the stop is 36
+#Your program prints one number instead of 10.
 numbers = random.randrange(25,35)
 for i in [numbers]:
     print (i)
